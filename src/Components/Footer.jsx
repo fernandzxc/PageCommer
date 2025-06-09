e30201491f30cec9661d666b285253aa0b43e054
fernandzxc <alifernandopinto@gmail.com>
2025-06-09 15:53:27 -0400

    add Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,14 +4,43 @@ import { BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-700 w-full ">
-      <div className="flex flex-col p-5"> 
-        <h1 className="text-lg text-white">Redes Sociales</h1>
+    <div className="flex justify-around bg-gray-700 w-full p-5">
+      <div>
+        <h1 className="text-lg text-gray-300 pb-2">REDES SOCIALES</h1>
         <figure className="flex gap-5">
-          <FaFacebook className="size-5 text-blue-600"/>
+          <FaFacebook className="size-5 text-[#1877F2]" />
           <GrInstagram className="size-5 text-fuchsia-600" />
           <BsTwitterX className="size-5" />
         </figure>
+      </div>
+
+      <div>
+        <h1 className="text-gray-300 text-lg pb-2">CATEGORIAS</h1>
+        <ul className="text-white flex flex-col gap-3">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Productos</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+          <li>
+            <a href="#">Preguntas Frecuentes</a>
+          </li>
+          <li>
+            <a href="#">Preguntas Frecuentes</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="text-gray-300 text-lg pb-2">CONTACTANOS</h1>
+        <ul className="text-white flex flex-col gap-3">
+          <li>5842456789875</li>
+          <li>pagecommerce@gmail.com</li>
+        </ul>
       </div>
     </div>
   );
